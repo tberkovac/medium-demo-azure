53e8b5c9872c3ab24733c5e61c54bd3d3fe185a1
tberkovac <tberkovac1@etf.unsa.ba>
2024-04-23 17:23:49 +0200

    Added database

--- medium-demo-azure/Program.cs
+++ medium-demo-azure/Program.cs
@@ -1,9 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using medium_demo_azure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connection = String.Empty;
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddEnvironmentVariables().AddJsonFile("appsettings.Development.json");
+    connection = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+}
+else
+{
+    connection = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
+}
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connection));
 
 var app = builder.Build();
 
@@ -19,8 +35,10 @@ app.UseHttpsRedirection();
 app.MapGet("/", () =>
 {
     return "Azure is great!";
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+});
+
+app.MapGet("/books", async (AppDbContext dbContext)=>{
+    return  await dbContext.Books.ToListAsync();
+});
 
 app.Run();
